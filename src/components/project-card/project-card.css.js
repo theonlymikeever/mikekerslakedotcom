f71dde6257cd153d9f39709ce91953c554c4529b
Mike Kerslake <theonlymikeever@gmail.com>
2018-10-19 15:23:00 -0400

    Ul > li indentation

--- src/components/project-card/project-card.css.js
+++ src/components/project-card/project-card.css.js
@@ -12,7 +12,7 @@ export const CardContainer = styled.div`
   /* box-shadow: 0 1px 3px rgba(0,0,0,0.09), 0 1px 2px rgba(0,0,0,0.15); */
 
   li {
-    margin: 0.24em;
+    margin: 0.5em;
   }
 
   li:before {
