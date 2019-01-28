e21093ac42d85f3f1179ee388f8a9bd171ea9c42
Mike Kerslake <theonlymikeever@gmail.com>
2019-01-28 17:51:47 -0500

    Update theme defaults

--- src/components/buttons/contact-btn.css.js
+++ src/components/buttons/contact-btn.css.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-// import { primary } from 'constants/theme';
+import { primary, light, white, shadowHover } from 'constants/theme';
 // import MEDIA from 'helpers/mediaTemplates';
 
 export const ButtonContainer = styled.div`
@@ -13,16 +13,16 @@ export const ButtonContainer = styled.div`
 export const Button = styled.a`
   grid-column-start: 1;
   grid-row-start: 1;
-  background: linear-gradient(to right, #00d2ff, #3a7bd5);
+  background: linear-gradient(to right, ${light}, ${primary});
   border-radius: 0.25rem;
   padding: 1rem;
   text-align: center;
-  color: #fff;
+  color: ${white};
   text-decoration: none;
   transition-duration: 0.3s;
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 
   &:hover {
-    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.1), 0 10px 10px rgba(0, 0, 0, 0.05);
+    box-shadow: ${shadowHover};
   }
 `;
--- src/constants/theme.js
+++ src/constants/theme.js
@@ -6,9 +6,16 @@ export const pink = '#c6426e';
 export const purple = '#642b73';
 export const red = '#f90000';
 export const grey = '#757575';
+export const white = '#ffffff';
 
 // Transparencies
 export const accentTrans = '#259BE499';
 export const pinkTrans = '#c6426e80';
 export const yellowTrans = '#EFBC6480';
 export const tealTrans = '#0DCFE699';
+
+// Shadows
+export const shadowLight =
+  '0 1px 3px rgba(0,0,0,0.12),0 1px 2px rgba(0,0,0,0.24)';
+export const shadowHover =
+  '0 14px 28px rgba(0, 0, 0, 0.1), 0 10px 10px rgba(0, 0, 0, 0.05)';
